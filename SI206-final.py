e58a76a1f2a2b759fa4e6e2a94247c6accef8c0d
llamonta <llamonta@umich.edu>
2021-12-08 15:20:50 -0500

    getstates with teams and get teams complete

--- SI206-final.py
+++ SI206-final.py
@@ -4,17 +4,31 @@ import json
 from bs4 import BeautifulSoup
 import requests
 import os
-#Names: Zack, Toby, Luke
+#Names: Zack, Toby, Luc
 
 def getStatesWithTeams():
     requestURL = "https://worldpopulationreview.com/state-rankings/nba-teams-by-state"
     master = []
     resp = requests.get(requestURL)
     soup = BeautifulSoup(resp.content, 'html.parser')
-    soup.find_all('tr' , class_='jsx-2636433790')
-    statename = soup.find_all( 'a', 'td')
-    for var in statename.text:
-        master.append(var)
-    return master
+    statename = soup.find_all( 'tbody', class_ = 'jsx-2636433790')
+    for var in statename:
+        x = var.find_all('a')
+        for variable in x:
+            master.append(variable.text)
+    print(master)
+
+def getNBATeams():
+    requestURL = "https://www.cbssports.com/nba/teams/"
+    master = []
+    resp = requests.get(requestURL)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    teamname1 = soup.find_all(class_ = "TeamName")
+    for team in teamname1:
+        x = team.find_all('a')
+        for variable in x:
+            master.append(variable.text)
+    print(master)
 
 getStatesWithTeams()
+getNBATeams()
