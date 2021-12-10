ed2dd36dfe621b47eb343e9bb70456f1d430f36b
tobyroth8 <89855181+tobyroth8@users.noreply.github.com>
2021-12-10 16:57:34 -0500

    Update SI206-final.py

--- SI206-final.py
+++ SI206-final.py
@@ -7,16 +7,22 @@ import os
 #Names: Zack, Toby, Luc
 
 def getStatesWithTeams():
-    requestURL = "https://worldpopulationreview.com/state-rankings/nba-teams-by-state"
+    requestURL = "https://worldpopulationreview.com/us-cities"
     master = []
+    master1 = []
     resp = requests.get(requestURL)
     soup = BeautifulSoup(resp.content, 'html.parser')
     statename = soup.find_all( 'tbody', class_ = 'jsx-2636433790')
     for var in statename:
-        x = var.find_all('a')
-        for variable in x:
-            master.append(variable.text)
-    print(master)
+        # x = var.find_all('a')
+        # for variable in x:
+        #     master.append(variable.text)
+        y = var.find_all('td')
+        for variable in y:
+            master1.append(variable.text)
+    print(master1)
+
+getStatesWithTeams()
 
 def getUSNBATeams():
     requestURL = "https://www.cbssports.com/nba/teams/"
